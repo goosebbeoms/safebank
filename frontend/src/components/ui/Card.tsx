d539bad8d953b64f25773038388d11450af962d3
goosebbeoms <seo980916@gmail.com>
2025-07-12 15:05:24 +0900

    fix: Card ui 컴포넌트 Props 인터페이스 수정

--- frontend/src/components/ui/Card.tsx
+++ frontend/src/components/ui/Card.tsx
@@ -4,7 +4,7 @@ import {clsx} from "clsx";
 interface CardProps {
     children: ReactNode;
     className?: string;
-    title: string;
+    title?: string;
 }
 
 export const Card = ({ children, className, title }: CardProps) => {

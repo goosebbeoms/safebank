078ba01df2d548a4f2d032cb3ace6e4f8745000d
goosebbeoms <seo980916@gmail.com>
2025-07-12 15:02:46 +0900

    feat: App.tsx 라우팅 초안 작성

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,21 @@
+import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom";
+import {Layout} from "./components/layout/Layout.tsx";
+import {DashboardPage} from "./pages/DashboardPage.tsx";
+
 function App() {
   return (
-    <h1>제목</h1>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          {/*<Route path="/members" element={} />*/}
+          {/*<Route path="/accounts" element={} />*/}
+          {/*<Route path="/transactions" element={} />*/}
+          {/*<Route path="*" element={} />*/}
+        </Routes>
+      </Layout>
+    </Router>
   )
 }
 

ced487ef9b19e2a22f1c60ffa776addd0a1675bd
goosebbeoms <seo980916@gmail.com>
2025-07-12 23:09:49 +0900

    feat: account service 구현

---
+++ frontend/src/services/accountService.ts
@@ -0,0 +1,20 @@
+import type {ApiResponse} from "../types/api.ts";
+import type {Account} from "../types/account.ts";
+
+export const accountService = {
+    getAll: (): Promise<ApiResponse<Account[]>> => (
+        api
+            .get('/accounts')
+            .then(res => res.data)
+    ),
+    getByNumber: (accountNumber: string): Promise<ApiResponse<Account>> => (
+        api
+            .get('/accounts/number/' + accountNumber)
+            .then(res => res.data)
+    ),
+    create: (data: AccountCreateRequest): Promise<ApiResponse<Account>> => (
+        api
+            .post('/accounts', data)
+            .then(res => res.data)
+    )
+}

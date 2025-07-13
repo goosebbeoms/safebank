4959c26d5a59c8012657645d32280745e032e5ad
goosebbeoms <seo980916@gmail.com>
2025-07-13 23:17:45 +0900

    feat: 거래 관련 서비스 코드 구현

---
+++ frontend/src/services/transactionService.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type { ApiResponse } from "../types/api";
+import type { Transaction, TransferRequest } from "../types/transaction";
+import api from "./api";
+
+export const transactionService = {
+    transfer: (data: TransferRequest): Promise<ApiResponse<Transaction>> => (
+        api
+            .post('/accounts/transfer', data)
+            .then(res => res.data)
+    ),
+    getByAccountNumber: (accountNumber: string, page = 0, size = 20): Promise<ApiResponse<any>> => (
+        api
+            .get(`/accounts/${accountNumber}/transactions?page=${page}&size=${size}`)
+            .then(res => res.data)
+    )
+}

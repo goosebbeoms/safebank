c3d17059a047a5e8e55bb394401475c4e5783104
goosebbeoms <seo980916@gmail.com>
2025-07-21 11:41:12 +0900

    feat: add spring security config allowed origin

--- backend/src/main/java/com/safebank/api/config/SecurityConfig.java
+++ backend/src/main/java/com/safebank/api/config/SecurityConfig.java
@@ -38,7 +38,8 @@ public class SecurityConfig {
 
         // 허용할 Origin 설정
         configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:5173"
+                "http://localhost:5173",
+                "http://localhost:8080"
         ));
 
         // 허용할 HTTP 메서드

f85b2cc21008700a41d131773ba4369c8dbc9801
goosebbeoms <seo980916@gmail.com>
2025-07-21 12:58:18 +0900

    feat: 대시보드 총 회원 수 구현

--- frontend/src/services/memberService.ts
+++ frontend/src/services/memberService.ts
@@ -1,32 +1,25 @@
-import type {ApiResponse} from "../types/api.ts";
-import type {Member, MemberCreateRequest} from "../types/member.ts";
+import type { ApiResponse } from "../types/api.ts";
+import type { Member, MemberCreateRequest } from "../types/member.ts";
 import api from "./api.ts";
-import type {Account} from "../types/account.ts";
+import type { Account } from "../types/account.ts";
 
 export const memberService = {
-    getAll: (): Promise<ApiResponse<Member[]>> => (
-        api
-            .get('/members')
-            .then(res => res.data)
-    ),
-    getById: (id: number): Promise<ApiResponse<Member>> => (
-        api
-            .get(`/members/${id}`)
-            .then(res => res.data)
-    ),
-    create: (data: MemberCreateRequest): Promise<ApiResponse<Member>> => {
-        console.log('start!!')
-        return api
-            .post('/members', data)
-            .then(res => {
-                console.log('complete!!!')
-                return res.data
-            })
-            .catch(err => console.error(err))
-    },
-    getAccount: (id: number): Promise<ApiResponse<Account[]>> => (
-        api
-            .get(`/members/${id}/accounts`)
-            .then(res => res.data)
-    )
-}
+  getAll: (): Promise<ApiResponse<Member[]>> =>
+    api.get("/members").then((res) => res.data),
+  getCount: (): Promise<ApiResponse<number>> =>
+    api.get("/members/count").then((res) => res.data),
+  getById: (id: number): Promise<ApiResponse<Member>> =>
+    api.get(`/members/${id}`).then((res) => res.data),
+  create: (data: MemberCreateRequest): Promise<ApiResponse<Member>> => {
+    console.log("start!!");
+    return api
+      .post("/members", data)
+      .then((res) => {
+        console.log("complete!!!");
+        return res.data;
+      })
+      .catch((err) => console.error(err));
+  },
+  getAccount: (id: number): Promise<ApiResponse<Account[]>> =>
+    api.get(`/members/${id}/accounts`).then((res) => res.data),
+};
